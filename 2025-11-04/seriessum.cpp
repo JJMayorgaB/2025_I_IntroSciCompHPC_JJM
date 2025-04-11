2479333a2c03536b828dfc8245bfefd85223669f
Juan Jose <jmayorgab@unal.edu.co>
2025-04-11 12:51:08 -0500

    files ordered

--- 2025-11-04/seriessum.cpp
+++ 2025-11-04/seriessum.cpp
@@ -9,13 +9,13 @@ REAL sum3(int k);
 
 int main(int argc, char **argv)
 {
-    std::cout.precision(16);
+    std::cout.precision(7);
     std::cout.setf(std::ios::scientific);
 
     int N = std::atoi(argv[1]);
 
     for(int k = 1; k <= N; k++)
-        std::cout << k << "\t" << sum1(k) << "\t" << sum2(k) << "\t" << sum3(k) << "\n";
+        std::cout << k << "\t" << sum1(k) << "\t" << sum2(k) << "\t" << sum3(k) << "\t" << std::fabs(sum1(k) - sum3(k))/sum3(k) << "\t" << std::fabs(sum2(k) - sum3(k))/sum3(k) <<"\n";
     return 0;
 
 }
@@ -24,7 +24,8 @@ REAL sum1(int k)
 {
     REAL suma = 0.0;
     for(int ii = 1; ii <= k; ii++){
-        suma += pow(-1, ii)*ii/(ii+1);
+        REAL n = ii*1.0;
+        suma += pow(-1, n)*n/(n+1);
     }
     return suma;
 }
@@ -34,8 +35,9 @@ REAL sum2(int k)
     REAL suma1 = 0.0;
     REAL suma2 = 0.0;
     for(int ii = 1; ii <= k; ii++){
-        suma1 += -(2*ii-1)/2*ii;
-        suma2 += 2*ii/(2*ii+1);
+        REAL n = ii*1.0;
+        suma1 += -(2*n-1)/2*ii;
+        suma2 += 2*n/(2*n+1);
     }
     return suma2 - suma1;
 }
@@ -44,7 +46,8 @@ REAL sum3(int k)
 {
     REAL suma = 0.0;
     for(int ii = 1; ii <= k; ii++){
-        suma += 1/(2*ii*(2*ii+1));
+        REAL n = ii*1.0;
+        suma += 1/(2*n*(2*n+1));
     }
     return suma;
 }
