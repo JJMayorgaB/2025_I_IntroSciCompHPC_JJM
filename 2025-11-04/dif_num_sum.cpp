2f604e0959d59f2e49dc8b446171e3c65d9da360
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2025-04-12 21:27:38 -0500

    pruebas hechas

--- 2025-11-04/dif_num_sum.cpp
+++ 2025-11-04/dif_num_sum.cpp
@@ -1,3 +1,7 @@
+//Al momento de utilizar este codigo desde consola recomiendo ejecutar de la forma .\a.exe N > data.txt, donde N
+//es el numero de sumas a realizar, esto con el fin de optimizar mejor el rendimiento del codigo y poder realizar
+//mas pruebas con distintos N de forma practica.
+
 #include <iostream>
 #include <cmath>
 
@@ -9,12 +13,12 @@ REAL sum2(int k);
 
 int main(int argc, char **argv)
 {
-    std::cout.precision(10);
-    std::cout.setf(std::ios::scientific);
+    std::cout.precision(16); 
+    std::cout.setf(std::ios::scientific); //La salida de las sumas se da en notación cientifca
 
-    int N = std::atoi(argv[1]);
+    int N = std::atoi(argv[1]); //N es el numero de sumas a realizar, ingrese este valor desde la consola
 
-    for(int k = 1; k <= N; k++){
+    for(int k = 1; k <= N; k++){ //Salida de los datos
         std::cout << k << "\t" 
                     << sum1(k) << "\t" 
                     << sum2(k) << "\t" 
@@ -29,7 +33,7 @@ REAL sum1(int k)
     REAL suma = 0.0;
     for(int ii = 1; ii <= k; ii++){
         REAL n = ii*1.0;
-        suma += 1/n;
+        suma += 1.0/n;
     }
     return suma;
 }
@@ -39,7 +43,7 @@ REAL sum2(int k)
     REAL suma = 0.0;
     for(int ii = k; ii >= 1; ii--){
         REAL n = ii*1.0;
-        suma += 1/n;
+        suma += 1.0/n;
     }
     return suma;
 }
