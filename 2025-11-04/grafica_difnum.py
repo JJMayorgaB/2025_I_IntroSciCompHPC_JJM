2ed632acba1e33e630df59b58018ebc3da2879fb
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2025-04-13 10:16:20 -0500

    tarea terminada

--- 2025-11-04/grafica_difnum.py
+++ 2025-11-04/grafica_difnum.py
@@ -10,11 +10,11 @@ k = data[:, 0]
 error = data[:, 3]
 
 plt.figure(figsize=(8, 6))
-plt.plot(k, error, 'ro', markersize=1, label=r'$|1 - \frac{\mathrm{sum}_1(k)}{\mathrm{sum}_2(k)}|$')
+plt.plot(k, error, 'ro', markersize=1, label=r'$\Delta = \left|1 - \frac{S_1(N)}{S_2(N)}\right|$')
 
 plt.xlabel(r'$N$', fontsize=14)
 plt.ylabel(r'Error relativo $\Delta$', fontsize=14)
-plt.title(r'Error entre $\mathrm{sum}_1(N)$ y $\mathrm{sum}_2(N)$', fontsize=16)
+plt.title(r'Error entre $S_1(N)$ y $S_2(N)$', fontsize=16)
 #plt.yscale('log') 
 #plt.xscale('log') 
 plt.grid(True, which='both', linestyle='--', alpha=0.5)
